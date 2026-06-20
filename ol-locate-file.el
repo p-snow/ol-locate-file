76f003eb72c431a45164aeaea3beefb31daec6e0
p-snow <public@p-snow.org>
2026-06-20 20:40:03 +0900

    build(release): bump version to 0.1.1

--- ol-locate-file.el
+++ ol-locate-file.el
@@ -7,7 +7,7 @@
 ;; Keywords: hypermedia, convenience
 ;; URL: https://github.com/p-snow/ol-locate-file
 ;; Package-Requires: ((emacs "30.1") (org "9.3"))
-;; Version: 0.1.0
+;; Version: 0.1.1
 
 ;; This file is NOT part of GNU Emacs.
 
